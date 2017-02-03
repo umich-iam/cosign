16cb4dc46aecbf3b96464484c193a51187570ac8
Brian Rahn <bdrahn@cosign-dev.dsc.umich.edu>
2017-02-03 16:02:54 -0500

    Fix bug when the destination URL has a space

--- cgi/cgi.c
+++ cgi/cgi.c
@@ -408,22 +408,17 @@ main( int argc, char *argv[] )
     CGIHANDLE			*cgi;
     struct uservarlist		*uv = NULL;
 
-    if ( argc == 2 ) {
-	if ( strcmp( argv[ 1 ], "-V" ) == 0 ) {
-	    printf( "%s\n", cosign_version );
-	    exit( 0 );
-	}
-
-	/*
-	 * the second argument is otherwise the query string per RFC 3875:
-	 *	http://tools.ietf.org/html/rfc3875#section-4.4
-	 *
-	 * just ignore it and use the QUERY_STRING env variable.
-	 */
-    } else if ( argc != 1 ) {
-	fprintf( stderr, "usage: %s [-V]\n", argv[ 0 ] );
-	exit( 1 );
+    if ( argc == 2 && strcmp( argv[ 1 ], "-V" ) == 0 ) {
+	printf( "%s\n", cosign_version );
+	exit( 0 );
     }
+
+    /*
+     * cosign.cgi may be called with the query string per RFC 3875:
+     *	http://tools.ietf.org/html/rfc3875#section-4.4
+     *
+     * but that's not required, so we use the QUERY_STRING env variable.
+     */
 
     if (( cosign_conf = getenv( "COSIGN_CGI_CONF" )) == NULL ) {
 	cosign_conf = _COSIGN_CONF;
